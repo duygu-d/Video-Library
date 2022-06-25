1511a313e50ae0435557409034588a3a983c1956
duygu-duran <dduran@technologica.com>
2022-06-25 09:13:24 +0300

    Fixed Statistics logic, added serialization

--- Video_Library/Statistics.cs
+++ Video_Library/Statistics.cs
@@ -15,10 +15,8 @@ namespace Video_Library
 {
     public partial class Statistics : Form
     {
-        private StatisticsDetails _statistics;
         private string requestResult;
         private List<StatisticsDetails> _allStatistics = new List<StatisticsDetails>();
-        private List<string> _videoURLs = new List<string>();
         private List<StatisticsDetails> _videoStatistics = new List<StatisticsDetails>();
         private Video _video;
         private VideoDetailsRegex _videoDetailsRegex;
@@ -41,47 +39,27 @@ namespace Video_Library
 
         private void Statistics_Load(object sender, EventArgs e)
         {
-            string[] lines = File.ReadAllLines(@"C:\Users\Laptop\Desktop\Video_Library\Video_Library\bin\Debug\statistics.txt");
-
-            if (lines.Length != 0)
+            if (GetAllStatisticsDetails() != null)
             {
-                for (int i = 0; i < lines.Length; i++)
+                _allStatistics = GetAllStatisticsDetails();
+            }
+
+            StatisticsDetails statistics = CreateStatistics(_video.URL);
+
+            if (_allStatistics.Count != 0)
+            {
+                for (int i = 0; i < _allStatistics.Count; i++)
                 {
-                    string[] line = lines[i].Split('\t');
-                    _videoURLs.Add(line[0]);
-                    StatisticsDetails lineStatistics = new StatisticsDetails(line[1], long.Parse(line[2]), long.Parse(line[3]));
-                    if (_video.URL == line[0])
+                    if (_allStatistics[i].Date.Equals(statistics.Date) && _allStatistics[i].VideoUrl.Equals(statistics.VideoUrl))
                     {
-                        _videoStatistics.Add(lineStatistics);
+                        _allStatistics.Remove(_allStatistics[i]);
                     }
-                    _allStatistics.Add(lineStatistics);
-
                 }
             }
 
-            var dateTime = DateTime.Now;
-            string date = dateTime.ToShortDateString();
-            long views = GetCurrentViews(_video.URL);
-            long likes = GetCurrentLikes(_video.URL);
+            _allStatistics.Add(statistics);
 
-            _statistics = new StatisticsDetails(date, views, likes);
-            #region ListItemsRefactoring
-            for (int i = 0; i < _videoURLs.Count; i++)
-            {
-                if (_videoURLs[i] == _video.URL)
-                {
-                    var repeated = _allStatistics.Where(s => s.Date == date).FirstOrDefault();
-                    _allStatistics.Remove(repeated);
-                    _videoStatistics.Remove(repeated);
-                    _videoURLs.Remove(_videoURLs[i]);
-                }
-            }
-            #endregion
-            _videoURLs.Add(_video.URL);
-            _allStatistics.Add(_statistics);
-            _videoStatistics.Add(_statistics);
-
-
+            _videoStatistics = VideoStatisticsDetails(_video.URL);
 
             for (int i = 0; i < _videoStatistics.Count; i++)
             {
@@ -93,14 +71,14 @@ namespace Video_Library
             }
         }
 
-        private long GetCurrentViews(string videoUrl)
+        private long GetCurrentViews(string request)
         {
-            string views = _videoDetailsRegex.GetViews(requestResult);
+            string views = _videoDetailsRegex.GetViews(request);
             return FormatString(views);
         }
-        private long GetCurrentLikes(string videoUrl)
+        private long GetCurrentLikes(string request)
         {
-            string likes = _videoDetailsRegex.GetLikes(requestResult);
+            string likes = _videoDetailsRegex.GetLikes(request);
             return FormatString(likes);
         }
 
@@ -118,19 +96,46 @@ namespace Video_Library
 
         private void Statistics_FormClosing(object sender, FormClosingEventArgs e)
         {
-            using (StreamWriter statisticsWrite = new StreamWriter(@"C:\Users\Laptop\Desktop\Video_Library\Video_Library\bin\Debug\statistics.txt"))
+            IFormatter historyFormatter = new BinaryFormatter();
+            using (var fs = new FileStream("allStatisticsData", FileMode.Create))
             {
-                for (int i = 0; i < _allStatistics.Count; i++)
-                {
-                    statisticsWrite.WriteLine($"{_videoURLs[i]}\t{_allStatistics[i].Date}\t{_allStatistics[i].Views}\t{_allStatistics[i].Likes}");
-                }
+                historyFormatter.Serialize(fs, _allStatistics);
             }
         }
 
+        private List<StatisticsDetails> VideoStatisticsDetails(string videoUrl)
+        {
+            return _allStatistics.Where(s => s.VideoUrl == videoUrl).ToList();
+        }
         private void okButton_Click(object sender, EventArgs e)
         {
             okButton.DialogResult = DialogResult.OK;
             this.Close();
+        }
+
+        private StatisticsDetails CreateStatistics(string videoUrl)
+        {
+            var dateTime = DateTime.Now;
+            string date = dateTime.ToShortDateString();
+            long views = GetCurrentViews(requestResult);
+            long likes = GetCurrentLikes(requestResult);
+
+            StatisticsDetails statistics = new StatisticsDetails(videoUrl, date, views, likes);
+
+            return statistics;
+        }
+        private List<StatisticsDetails> GetAllStatisticsDetails()
+        {
+            if (!File.Exists("allStatisticsData"))
+            {
+                return null;
+            }
+
+            IFormatter statisticsFormatter = new BinaryFormatter();
+            using (var fs = new FileStream("allStatisticsData", FileMode.Open))
+            {
+                return _allStatistics = (List<StatisticsDetails>)(statisticsFormatter.Deserialize(fs));
+            }
         }
     }
 }
--- Video_Library/StatisticsDetails.cs
+++ Video_Library/StatisticsDetails.cs
@@ -6,14 +6,17 @@ using System.Threading.Tasks;
 
 namespace Video_Library
 {
+    [Serializable]
     public class StatisticsDetails
     {
+        public string VideoUrl { get; set; }
         public string Date { get; set; }
         public long Views { get; set; }
         public long Likes { get; set; }
 
-        public StatisticsDetails(string date, long views, long likes)
+        public StatisticsDetails(string videoUrl, string date, long views, long likes)
         {
+            VideoUrl = videoUrl;
             Date = date;
             Views = views;
             Likes = likes;
